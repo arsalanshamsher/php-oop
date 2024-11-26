0de9659710c565dc057b6bd8c49ccc76f453564d
arsalanshamsher <skstudios876@gmail.com>
2024-11-26 12:22:58 +0500

    Update helper.php
    update view function.
    add allow parms

--- app/helper.php
+++ app/helper.php
@@ -9,15 +9,17 @@ function redirect($url)
 // end of redirect function
 
 // view directory function
-function view($dir)
+function view($dir, $data = [], $defaultData = [])
 {
+    $data = array_merge($data,$defaultData);
+    extract($data);
     $dir = str_replace('\\', DIRECTORY_SEPARATOR, $dir);
     $dir = str_replace('.', DIRECTORY_SEPARATOR, $dir);
     $file = APP_ROOT . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . $dir . '.php';
     if (file_exists($file)) {
         return require $file;
     }
-    throw new Exception('Page not found. '. $file);
+    throw new Exception('View not found. '. $file);
 }
 // end of view directory function
 

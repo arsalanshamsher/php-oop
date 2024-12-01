35f3bcdf0f551595871a74a986b2ae4333ee3f98
arsalanshamsher <skstudios876@gmail.com>
2024-12-01 17:14:33 +0500

    add all method in model class
    all method is fatch all data

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -3,5 +3,13 @@ namespace App\Models;
 use App\Config\Database;
 
 class Model extends Database{
-    
+    protected static $table;
+
+    // Static method to fetch all data
+    public static function all() {
+        $table = static::$table; // Access the table property
+        $sql = new Database();
+        $sql->select($table);
+        return $sql->getResult();
+    }
 }
